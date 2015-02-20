08de1be5b8558ff5986cb47610865a78ff5b2725
Luke Rodgers <lukeasrodgers@gmail.com>
2015-02-19 21:54:04 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate mio;
+
+
+use mio::*;
+use mio::net::{SockAddr};
+use mio::net::tcp::{TcpSocket, TcpAcceptor};
+use std::thread::Thread;
+use std::old_io;
+
 fn main() {
-    println!("Hello, world!");
+    struct EchoHandler;
+
+    impl Handler<(), String> for EchoHandler {
+        fn notify(&mut self, event_loop: &mut EventLoop<(), String>, msg: String) {
+            print!("{}", msg);
+            if msg.as_slice() == "done" {
+                event_loop.shutdown();
+            }
+        }
+    }
+    let mut event_loop = EventLoop::new().unwrap();
+    let sender = event_loop.channel();
+
+    Thread::spawn(move || {
+        for line in old_io::stdin().lock().lines() {
+            let _ = sender.send(line);
+        }
+    });
+
+    let _ = event_loop.run(EchoHandler);
 }
