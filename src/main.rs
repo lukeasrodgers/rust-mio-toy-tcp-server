f382bf5dfd60f91b0e1d107a20603b69e2920512
Luke Rodgers <lukeasrodgers@gmail.com>
2015-02-22 23:32:40 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,62 @@ extern crate mio;
 use mio::*;
 use mio::net::{SockAddr};
 use mio::net::tcp::{TcpSocket, TcpAcceptor};
-use std::thread::Thread;
-use std::old_io;
+use mio::util::Slab;
 
 fn main() {
-    struct EchoHandler;
-    impl Handler<(), String> for EchoHandler {
-        fn notify(&mut self, event_loop: &mut EventLoop<(), String>, msg: String) {
-            print!("{}", msg);
-            if msg.as_slice() == "done\n" {
-                event_loop.shutdown();
+    const SERVER: Token = Token(1);
+    let addr = SockAddr::parse("127.0.0.1:13265").unwrap();
+    let server = TcpSocket::v4().unwrap()
+        .bind(&addr).unwrap()
+        .listen(256).unwrap();
+
+    let mut event_loop = EventLoop::<(), ()>::new().unwrap();
+    event_loop.register(&server, SERVER).unwrap();
+
+    struct TcpHandler {
+        conns: Slab<TcpSocket>,
+        sock: TcpAcceptor
+    };
+
+    impl TcpHandler {
+        fn accept(&mut self, event_loop: &mut EventLoop<(), ()>) {
+            let conn = self.sock.accept();
+            let sock = conn.unwrap().unwrap();
+            let tok = self.conns.insert(sock).ok().expect("could not add connection to slab");
+            event_loop.register_opt(&self.conns[tok], tok, Interest::readable(), PollOpt::edge() | PollOpt::oneshot());
+        }
+    }
+
+    impl Handler<(), ()> for TcpHandler {
+        fn readable(&mut self, event_loop: &mut EventLoop<(), ()>, token: Token, _: ReadHint) {
+            match token {
+                SERVER => {
+                    self.accept(event_loop);
+                }
+                tok => {
+                    println!("tok: {}", tok.as_usize());
+                    let mut read_buf = [0, 4096];
+                    match self.conns[tok].read(&mut read_buf) {
+                        Ok(NonBlock::WouldBlock) => {
+                            panic!("We just got readable, but were unable to read from the socket?");
+                        }
+                        Ok(NonBlock::Ready(r)) => {
+                            println!("CONN : we read {} bytes!", r);
+                            self.interest.remove(Interest::readable());
+                            self.interest.insert(Interest::writable());
+                        }
+                        Err(e) => {
+                            println!("not implemented; client err={:?}", e);
+                            self.interest.remove(Interest::readable());
+                        }
+                    }
+                }
             }
         }
     }
-    let mut event_loop = EventLoop::new().unwrap();
-    let sender = event_loop.channel();
-    // Send the notification from another thread
-    Thread::spawn(move || {
-        let mut stdin = old_io::stdin();
-        loop {
-            for line in stdin.lock().lines() {
-                sender.send(line.unwrap());
-            }
-        }
-    });
-
-    event_loop.run(EchoHandler);
+    let mut tcp_server = TcpHandler {
+        conns: Slab::new_starting_at(Token(2), 128),
+        sock: server
+    };
+    let _ = event_loop.run(&mut tcp_server);
 }
