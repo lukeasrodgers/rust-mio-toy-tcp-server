ec74d11019b33856204ee638bce46e74f26ac237
Luke Rodgers <lukeasrodgers@gmail.com>
2015-02-24 20:54:33 -0500

    More simplification, adding comments.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,54 @@
+/*
+ * Simple tcp listener, similar to `nc -l 13265`.
+ * Will listen for TCP connections on port 13265, accept
+ */
 extern crate mio;
 
 use mio::*;
 use mio::net::{SockAddr};
 use mio::net::tcp::{TcpSocket, TcpAcceptor};
-use mio::buf::{ByteBuf, MutByteBuf, SliceBuf};
+use mio::buf::{ByteBuf};
 
 fn main() {
     const SERVER: Token = Token(1);
     let addr = SockAddr::parse("127.0.0.1:13265").unwrap();
+    // Listen on `addr`, with a connection backlog of just 1, since we only want to handle one
+    // connection. See `man listen`.
     let server = TcpSocket::v4().unwrap()
         .bind(&addr).unwrap()
-        .listen(256).unwrap();
+        .listen(1).unwrap();
 
     let mut event_loop = EventLoop::<(), ()>::new().unwrap();
     event_loop.register(&server, SERVER).unwrap();
 
     struct TcpHandler {
-        conn: Option<TcpSocket>,
-        sock: TcpAcceptor
+        conn: Option<TcpSocket>, // Will store our single socket connection, once established.
+        sock: TcpAcceptor // Will store our server.
     };
 
     impl TcpHandler {
         fn accept(&mut self, event_loop: &mut EventLoop<(), ()>) {
+            match self.conn {
+                Some(_) => {
+                    // If we already have a connection, do nothing.
+                    return;
+                },
+                None => {}
+            }
             let conn = self.sock.accept();
             let sock = conn.unwrap().unwrap();
             let tok = Token(2);
+            // The following code will move `sock` into our `tcp_server` struct,
+            // so we won't be able to access it directly anymore.
             self.conn = Some(sock);
             match self.conn {
-                Some(ref c) => { event_loop.register_opt(c, tok, Interest::readable(), PollOpt::edge() | PollOpt::oneshot()); }
+                Some(ref c) => {
+                    // Register an interest read events with the event_loop. the `edge` option
+                    // stipulates that we will only be notified when a read event happens, rather
+                    // than as long as reading is possible.
+                    // See http://en.wikipedia.org/wiki/Epoll
+                    let _ = event_loop.register_opt(c, tok, Interest::readable(), PollOpt::edge());
+                }
                 None => { }
             }
         }
@@ -37,38 +58,45 @@ fn main() {
         fn readable(&mut self, event_loop: &mut EventLoop<(), ()>, token: Token, _: ReadHint) {
             match token {
                 SERVER => {
+                    // Call `accept` on our `tcp_handler`.
                     self.accept(event_loop);
                 }
-                tok => {
-                    println!("tok: {}", tok.as_usize());
+                Token(2) => {
+                    // Artificial limitation -- we'll only read up to 2048 bytes at a time.
                     let mut read_buf = ByteBuf::mut_with_capacity(2048);
-                    let mut interest = Interest::readable();
                     match self.conn {
                         Some(ref c) => {
                             match c.read(&mut read_buf) {
                                 Ok(NonBlock::WouldBlock) => {
-                                    panic!("We just got readable, but were unable to read from the socket?");
+                                    panic!("Read operation would block, bailing cuz this shouldn't happen.");
                                 }
-                                Ok(NonBlock::Ready(r)) => {
+                                Ok(NonBlock::Ready(_)) => {
+                                    // `_` would be the number of bytes read.
+                                    // `flip` will return a `ByteBuf` on which we can call
+                                    // `read_slice` to get the data available to be read.
+                                    // See http://carllerche.github.io/bytes/bytes/struct.ByteBuf.html
                                     let mut buf = read_buf.flip();
                                     let mut sl = [0; 2048];
                                     buf.read_slice(&mut sl);
+                                    // Assuming what was written was encoded as UTF8, print what
+                                    // was read to STDOUT.
                                     print!("{}", String::from_utf8(sl.to_vec()).unwrap());
-                                    // self.interest.remove(Interest::readable());
-                                    // self.interest.insert(Interest::writable());
                                 }
                                 Err(e) => {
-                                    event_loop.shutdown();
-                                    // println!("not implemented; client err={:?}", e);
-                                    // interest = Interest::hup();
-                                    // self.interest.remove(Interest::readable());
+                                    if e.is_eof() {
+                                        println!("Client closed connection, shutting down.");
+                                        event_loop.shutdown();
+                                    }
+                                    else {
+                                        panic!(e);
+                                    }
                                 }
                             }
-                            event_loop.reregister(c, tok, interest, PollOpt::edge() | PollOpt::oneshot());
                         },
                         None => { }
                     }
-                }
+                },
+                _ => { panic!("received token we can't handle".to_string()) }
             }
         }
     }
